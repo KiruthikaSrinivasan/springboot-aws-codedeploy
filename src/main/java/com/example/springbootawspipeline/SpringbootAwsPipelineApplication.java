6c1936f1593e21c8bf8132e9757faaf0cc2c2a1f
Kiruthika <kiruthikas98@gmail.com>
2023-11-21 13:11:15 +0530

    first commit

---
+++ src/main/java/com/example/springbootawspipeline/SpringbootAwsPipelineApplication.java
@@ -0,0 +1,20 @@
+package com.example.springbootawspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAwsPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsPipelineApplication.class, args);
+	}
+	@GetMapping("/")
+	public String getData()
+	{
+		return "HELLO KIRUTHIKA FROM AMIZHTH";
+	}
+}
---
+++ src/test/java/com/example/springbootawspipeline/SpringbootAwsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootawspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
